5fd1b754b7a873063b400461c4d02b0228f481e0
petruki <31597636+petruki@users.noreply.github.com>
2020-11-25 17:23:22 -0800

    Init commit

---
+++ src/main/java/github/petruki/ApiResources.java
@@ -0,0 +1,30 @@
+package github.petruki;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.security.PermitAll;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jose4j.json.internal.json_simple.JSONObject;
+
+@Path("/api")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ApiResources {
+
+    @GET
+    @PermitAll
+    @Path("/check")
+    public Response check() {
+    	Map<String, String> result = new HashMap<>();
+    	result.put("result", "All good");
+        return Response.ok(JSONObject.toJSONString(result)).build();
+    }
+    
+}
---
+++ src/test/java/github/petruki/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package github.petruki;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
